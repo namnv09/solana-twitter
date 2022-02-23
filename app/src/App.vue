c3d929eece53079da0e4c0ef144e5a949b0fa938
namnv09 <nam.nguyen@teqie.io>
2022-02-23 14:02:31 +0700

    app: Use wallet UI components

--- app/src/App.vue
+++ app/src/App.vue
@@ -2,7 +2,17 @@
 import { useRoute } from 'vue-router'
 import TheSidebar from './components/TheSidebar'
 
+import { PhantomWalletAdapter, SolflareWalletAdapter } from '@solana/wallet-adapter-wallets'
+import { initWallet } from 'solana-wallets-vue'
+
 const route = useRoute()
+
+const wallets = [
+    new PhantomWalletAdapter(),
+    new SolflareWalletAdapter(),
+]
+
+initWallet({ wallets, autoConnect: true })
 </script>
 
 <template>
